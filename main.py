dc59ad5a62ae26e02baf841f8be9ca3e9f546df2
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2024-05-26 20:47:22 +0500

    add tree

--- main.py
+++ main.py
@@ -21,22 +21,45 @@ def get_result(a, b, op):
 	return None
 
 
-ops = get_random_number(1, 3)
-print(ops)
+tree = {}
+tree['v'] = get_random_op()
 
-a = get_random_number()
-b = get_random_number()
-op = get_random_op()
-equation = f'{a} {op} {b}'
+# DFS
 
-res = get_result(a, b, op)
+stack = [(tree, 1)]
 
-for i in range(1, ops):
-	c = get_random_number()
-	op = get_random_op()
+# number of parentheses
+MAX_DEPTH = 2
 
-	equation = f'({equation}) {op} {c}'
-	res = get_result(res, c, op)
+while stack:
+	node, depth = stack.pop()
 
+	for d in ['left', 'right']:
+		node[d] = {}
+		thing = random.choice(['op', 'num'])
+		if thing == 'op' and depth <= MAX_DEPTH:
+			node[d]['v'] = get_random_op()
+			stack.append((node[d], depth + 1))
+		else:
+			node[d]['v'] = get_random_number()
+
+
+def get_equation(tree):
+	if tree['v'] in ['+', '-', '*', '/']:
+		return f'({get_equation(tree["left"])} {tree["v"]} {get_equation(tree["right"])})'
+	return tree['v']
+
+
+def get_tree_result(tree):
+	if tree['v'] in ['+', '-', '*', '/']:
+		return get_result(get_tree_result(tree['left']),
+		                  get_tree_result(tree['right']), tree['v'])
+	return tree['v']
+
+
+# debug
+print(tree)
+equation = get_equation(tree)[1:-1]
 print(equation)
-print(res)
+print('tree traversal', get_tree_result(tree))
+print('eval(equation)', eval(equation))
